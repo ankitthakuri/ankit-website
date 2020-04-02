23c9e91afcbdc4a3daab03bab9dc3e24b3a64a02
ankit thakuri <ankit96.thakuri@gmail.com>
2020-04-02 13:48:50 +0545

    Forst Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>Creative Dev Bootstrap portfolio web template</title>
+
+    <!-- Mobile Specific Metas
+  ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- CSS
+  ================================================== -->
+    <!-- Bootstrap -->
+    <link href="/theme/assets/css/bootstrap.min.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="/theme/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="/theme/css/animate.css"> 
+    <link rel="stylesheet" href="/theme/css/prettyPhoto.css"> 
+    <link rel="stylesheet" href="/theme/css/style.css"> 
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+   <body data-spy="scroll" data-target=".main-nav">
+    
+    <section id="section-banner">
+	<div class="pattern-overlay"></div>
+    	<div class="container">
+    		<div class="row">
+    			<div class="banner-content wow fadeInRight">
+    				<h2 class="title"> 
+	    					<span>Hello..!</span>
+							
+				<span>I'm Ankit Thakuri</span>
+							CREATIVE WEB DEVELOPER
+    				</h2>
+
+    				<a href="#" class="btn btn-default">Learn More</a>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- initialize jQuery Library -->
+    <script type="text/javascript" src="/theme/js/jquery.js"></script>
+    <!-- Bootstrap jQuery -->
+    <script type="text/javascript" src="/theme/assets/js/bootstrap.min.js"></script>
+    <!-- PrettyPhoto -->
+    <script type="text/javascript" src="/theme/js/jquery.prettyPhoto.js"></script>
+    <!-- Wow Animation -->
+    <script type="text/javascript" src="/theme/js/wow.min.js"></script>
+    <!-- singlepagenav -->
+    <script src="/theme/js/jquery.singlePageNav.js"></script>
+    <!-- Eeasing -->
+    <script type="text/javascript" src="/theme/js/jquery.easing.1.3.js"></script>
+    <!-- Sticky Menu -->
+    <script src="/theme/js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="/theme/js/custom.js"></script> 
+	<script>
+		 $(".main-nav").sticky();
+	</script>
+
+	<script> 
+        new WOW().init();
+    
+    </script>
+  </body>
+</html>
